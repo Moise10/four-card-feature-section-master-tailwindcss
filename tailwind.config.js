b968ad176718cc5246b0c8e9a4244cd293cd83af
moise <mbakopmoise237@gmail.com>
2022-05-17 04:44:42 +0800

    completed

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	content: ['./dist/index.html'],
+	theme: {
+		extend: {
+			boxShadow: {
+				'3xl': 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+			},
+			screens: {
+				sm: '640px',
+				// => @media (min-width: 640px) { ... }
+
+				md: '768px',
+				// => @media (min-width: 768px) { ... }
+
+				lg: '895px',
+				// => @media (min-width: 1024px) { ... }
+
+				xl: '1280px',
+				// => @media (min-width: 1280px) { ... }
+
+				'2xl': '1536px',
+				// => @media (min-width: 1536px) { ... }
+			},
+		},
+	},
+	plugins: [],
+};
