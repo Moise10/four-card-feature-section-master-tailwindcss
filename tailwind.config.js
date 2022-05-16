0c2576d9704864842ffaf2791221e4e3627de047
moise <mbakopmoise237@gmail.com>
2022-05-17 04:51:36 +0800

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,9 @@ module.exports = {
 				'2xl': '1536px',
 				// => @media (min-width: 1536px) { ... }
 			},
+			fontFamily: {
+				sans: ['Poppins', 'sans-serif'],
+			},
 		},
 	},
 	plugins: [],
